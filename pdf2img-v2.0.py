9b1d526f9a2953c4567da3ffa74f8fcfdb4391ef
AK Nahin <asrafuzzaman@iut-dhaka.edu>
2021-07-04 18:28:15 +0600

    Update pdf2img-v2.0.py

--- pdf2img-v2.0.py
+++ pdf2img-v2.0.py
@@ -3,7 +3,7 @@ from pdf2image import convert_from_path
 import os
 mode = 0o666
 a = 'y'
-print("PDF TO IMAGE CONVERTER 2.0\n\n")
+print("\n\n***PDF TO IMAGE CONVERTER 2.0***\n\n")
 print('***Write PDF file name with full directory if it is not in the same directory of this application')
 print('***write only file name name if both are in same folder')
 print('***Image will be saved in the same directory of the PDF')
