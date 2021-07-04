0429d63df0d80498ff8284059f50431aa0b1d1e0
AK Nahin <asrafuzzaman@iut-dhaka.edu>
2021-07-04 18:26:50 +0600

    Update pdf2img-v2.0.py

--- pdf2img-v2.0.py
+++ pdf2img-v2.0.py
@@ -6,7 +6,7 @@ a = 'y'
 print("PDF TO IMAGE CONVERTER 2.0\n\n")
 print('***Write PDF file name with full directory if it is not in the same directory of this application')
 print('***write only file name name if both are in same folder')
-print('***Image will be downloaded in the same directory of the application, not PDF')
+print('***Image will be downloaded in the same directory of the PDF')
 while a == 'y' or a == 'Y':
     pdf = input("\nPDF file name: ")
     dr = pdf.replace('.pdf', '')
